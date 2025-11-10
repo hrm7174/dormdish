e6fb05ddaa1e25237e40d5e93cd70cf70159f5c9
Heidy Mendez <heidymendez@Heidys-MacBook-Pro-6.local>
2025-11-09 23:09:18 -0500

    adding delete profile option

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,4 +1,6 @@
 class UserProfilesController < ApplicationController
+  before_action :set_user_profile, only: [:show, :edit, :update, :destroy]
+  
   def new
     @user_profile = UserProfile.new
   end
@@ -14,15 +16,14 @@ class UserProfilesController < ApplicationController
   end
 
   def show
-    @user_profile = UserProfile.find(params[:id])
+    # @user_profile is set by before_action
   end
 
   def edit
-    @user_profile = UserProfile.find(params[:id])
+    # @user_profile is set by before_action
   end
 
   def update
-    @user_profile = UserProfile.find(params[:id])
     if @user_profile.update(user_profile_params)
       redirect_to @user_profile, notice: "Profile updated successfully!"
     else
@@ -30,7 +31,29 @@ class UserProfilesController < ApplicationController
     end
   end
 
+  # NEW: Destroy action
+  def destroy
+    profile_name = @user_profile.name
+    
+    # Clear session if deleting current profile
+    if session[:user_profile_id] == @user_profile.id
+      session[:user_profile_id] = nil
+    end
+    
+    # Delete profile (will cascade delete meal_plans, shopping_lists)
+    @user_profile.destroy
+    
+    redirect_to root_path, notice: "Profile '#{profile_name}' and all associated data have been deleted."
+  end
+
   private
+
+  # NEW: Before action to set user profile
+  def set_user_profile
+    @user_profile = UserProfile.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Profile not found."
+  end
 
   def user_profile_params
     params.require(:user_profile).permit(:name, :weekly_budget, appliances: [], dietary_preferences: [])
