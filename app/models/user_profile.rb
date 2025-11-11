745e11c4776104ee5b630222eca10d7d5297d11d
Tobechi <tobechionwuka@gmail.com>
2025-11-11 15:23:39 -0500

    fixing the error message for profile validation

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -9,13 +9,13 @@ class UserProfile < ApplicationRecord
 
   def at_least_one_appliance
     if appliances.blank? || appliances.all?(&:blank?)
-      errors.add(:appliances, "must select at least one appliance")
+      errors.add(:base, "Must select at least one appliance")
     end
   end
 
   def at_least_one_dietary_preference
     if dietary_preferences.blank? || dietary_preferences.all?(&:blank?)
-      errors.add(:dietary_preferences, "must select at least one dietary preference")
+      errors.add(:base, "Must select at least one dietary preference")
     end
   end
 end
