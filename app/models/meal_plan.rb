ddfc96e4a2d77f39130788a02b721f0c8a926449
Heidy Mendez <heidymendez@Heidys-MacBook-Pro-6.local>
2025-12-01 17:55:30 -0500

    changes to fix delete profile error

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -2,14 +2,18 @@ class MealPlan < ApplicationRecord
   belongs_to :user_profile
   belongs_to :recipe
 
-  after_commit :update_shopping_list, on: [ :create, :destroy ]
+  after_commit :update_shopping_list, on: [:create, :destroy], if: :should_update_shopping_list?
 
   private
 
+  def should_update_shopping_list?
+    user_profile.present? && user_profile.persisted? && !user_profile.destroyed?
+  end
+
   def update_shopping_list
+    return unless user_profile&.persisted?
+    
     shopping_list = user_profile.shopping_lists.last || user_profile.shopping_lists.create
-
-
     shopping_list.generate_items
   end
 end
